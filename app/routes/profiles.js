7dac7bef3b5b3a1a01ee40e0c6e041924fd615b8
mateuspv <mateuspatzlaff@gmail.com>
2015-04-28 07:23:55 -0300

    update profiles route

--- app/routes/profiles.js
+++ app/routes/profiles.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model(_, transition) {
+  model(route, transition) {
     return this.store.find('profile', {
-      id: 1,
-      network: 'facebook'
+      id: route.id,
+      network: route.network
     });
   }
 });
