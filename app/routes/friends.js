da4bd8f5ac28f2fc9285ca909917799ba158c438
mateuspv <mateuspatzlaff@gmail.com>
2015-06-01 04:21:18 -0300

    added groups for facebook aprove

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -3,6 +3,12 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model() {
-		return this.store.find('friend');
+    let friends = this.store.find('friend');
+    let groups = this.store.findAll('group-facebook');
+		return Ember.RSVP.all([friends, groups])
+      .then((response) => {
+        let [friends, groups] = response;
+        return {friends,groups};
+      })
 	}
 });
