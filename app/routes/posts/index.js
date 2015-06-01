59cdb4f49e01340db2a3700bf4e4b1f20b10ff19
mateuspv <mateuspatzlaff@gmail.com>
2015-06-01 02:42:35 -0300

    fixed hashtags style

--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  renderTemplate(...args) {
+    this._super(this, ...args);
+    this.render('connect-filters', {
+      into: 'application',
+      outlet: 'filters',
+    });
+  },
   model() {
     return this.store.find('post');
   }
--- server/mocks/hashtag.js
+++ server/mocks/hashtag.js
@@ -5,11 +5,16 @@ module.exports = function(app) {
   hashtagRouter.get('/', function(req, res) {
     res.send({
       'hashtag': [
-        { id: 1, hashtag: 'RPG' },
-        { id: 2, hashtag: 'DOTA2' },
-        { id: 3, hashtag: 'TSI' },
-        { id: 4, hashtag: 'Pelotas' },
-        { id: 5, hashtag: '9GAG' },
+        { id: 0, hashtag: '#TheVoiceChile' },
+        { id: 1, hashtag: '#GameofThrones' },
+        { id: 2, hashtag: '#Superstar' },
+        { id: 3, hashtag: '#WWEChamber' },
+        { id: 4, hashtag: '#FPfecha14' },
+        { id: 5, hashtag: 'Querétaro'},
+        { id: 6, hashtag: 'Gago'},
+        { id: 7, hashtag: 'Velez'},
+        { id: 8, hashtag: 'Millonarios'},
+        { id: 9, hashtag: 'South West'},
       ]
     });
   });
