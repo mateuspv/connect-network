ef769affdee12781cab90b63fa6257d30fc3386a
mateuspv <mateuspatzlaff@gmail.com>
2015-06-01 15:48:09 -0300

    added android theme color

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,17 @@ import CurrentPage from '../mixins/current-page';
 import CurrentNetwork from '../mixins/current-network';
 
 export default Ember.Controller.extend(CurrentNetwork, CurrentPage, {
+  currentColor: function () {
+    let currentNetwork = this.get('currentNetwork');
+    if(currentNetwork === 'facebook') {
+      return '#3b5998';
+    }
+    if(currentNetwork === 'twitter') {
+      return '#55acee';
+    }
+    return '#1E88E2';
+  }.property('currentNetwork'),
+
   actions: {
     toggleSidebar() {
       this.toggleProperty('isDashboardEnabled');
